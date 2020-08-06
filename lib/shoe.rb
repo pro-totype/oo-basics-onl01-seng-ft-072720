d3a1ee780b8fdaf0453a462d8509c03e44389587
Sean B <butts.sean01@gmail.com>
2020-08-06 05:16:07 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,2 +1,39 @@
 # Make your shoe class here!
 class Shoe
+  
+    def shoe(shoe)
+      @shoe = shoe
+    end
+    
+    def shoe
+      @shoe
+    end
+    
+    def color=(color)
+      @color = color
+    end
+    
+    def color
+      @color
+    end
+    
+    def size=(num)
+      @size = num
+    end
+    
+    def size
+      @size
+    end
+    
+    def material=(material)
+      @material = material
+    end
+    
+    def material
+      @material
+    end
+    
+    def condition=()
+      
+      
+      
